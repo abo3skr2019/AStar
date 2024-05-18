ba5123a86b40c5ecd47481b2b0d8cfc6ca66a1fd
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2024-05-18 20:08:06 +0300

    refactor: Removed Unused CSV for snakeviz inst\ead

--- RunForData.py
+++ RunForData.py
@@ -1,21 +1,5 @@
 import cProfile
-import pstats
-import csv
 from A_Star_Implementation import visualize_search, generate_maze
-
-def write_pstats_to_csv(pstats_file, csv_file):
-    p = pstats.Stats(pstats_file)
-    p.sort_stats('cumulative')
-
-    with open(csv_file, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['ncalls', 'tottime', 'percall', 'cumtime', 'percall', 'filename:lineno(function)'])
-
-        for stat in p.stats.items():
-            func_name = stat[0]
-            func_stat = stat[1]
-            row = [func_stat[0], func_stat[2], func_stat[2]/func_stat[0], func_stat[3], func_stat[3]/func_stat[0], func_name]
-            writer.writerow(row)
 
 def run_and_profile():
     pregen_maze =[[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
@@ -34,11 +18,8 @@ def run_and_profile():
     end = (4, 6)
     if used_maze[end[0]][end[1]] == 1 or used_maze[start[0]][start[1]] == 1:
         print("End node is an obstacle or start node is an obstacle.")
-        return
     else:
         cProfile.runctx('visualize_search(used_maze, start, end)', globals(), locals(), 'output.pstats')
-
-    write_pstats_to_csv('output.pstats', 'pstats_output.csv')
 
 if __name__ == '__main__':
     run_and_profile()
